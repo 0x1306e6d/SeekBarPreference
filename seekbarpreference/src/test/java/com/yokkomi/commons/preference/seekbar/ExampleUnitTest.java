b7d2d649da1d86c1c27ede2f68824574d5ee7c4b
Gihwan Kim <ghkim3221@gmail.com>
2016-06-05 23:18:29 +0900

    Rename app module to example. Create seekbarpreference library module.

--- seekbarpreference/src/test/java/com/yokkomi/commons/preference/seekbar/ExampleUnitTest.java
+++ seekbarpreference/src/test/java/com/yokkomi/commons/preference/seekbar/ExampleUnitTest.java
@@ -14,4 +14,18 @@
  *    limitations under the License.
  */
 
-include ':example', ':seekbarpreference'
+package com.yokkomi.commons.preference.seekbar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
